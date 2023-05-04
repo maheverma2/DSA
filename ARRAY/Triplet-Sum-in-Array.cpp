ffd49ff2073bea605b489923c6066d17ef839074
maheverma2 <130764178+maheverma2@users.noreply.github.com>
2023-05-04 19:20:59 +0530

    Create Triplet-Sum-in-Array.cpp
    https://practice.geeksforgeeks.org/problems/triplet-sum-in-array-1587115621/1?page=1&difficulty[]=1&category[]=Arrays&curated[]=7&sortBy=submissions
    
    Given an array arr of size n and an integer X. Find if there's a triplet in the array which sums up to the given integer X.
    
    
    Example 1:
    
    Input:
    n = 6, X = 13
    arr[] = [1 4 45 6 10 8]
    Output:
    1
    Explanation:
    The triplet {1, 4, 8} in 
    the array sums up to 13.
    Example 2:
    
    Input:
    n = 5, X = 10
    arr[] = [1 2 4 3 6]
    Output:
    1
    Explanation:
    The triplet {1, 3, 6} in 
    the array sums up to 10.
    
    Your Task:
    You don't need to read input or print anything. Your task is to complete the function find3Numbers() which takes the array arr[], the size of the array (n) and the sum (X) as inputs and returns True if there exists a triplet in the array arr[] which sums up to X and False otherwise.
    
    
    Expected Time Complexity: O(n2)
    Expected Auxiliary Space: O(1)
    
    
    Constraints:
    1 ≤ n ≤ 103
    1 ≤ A[i] ≤ 105
    
    View Bookmarked Problems 
    Company Tags
    Topic Tags
    Related Courses
    Related Interview Experiences

---
+++ ARRAY/Triplet-Sum-in-Array.cpp
@@ -0,0 +1,27 @@
+class Solution{
+    public:
+    //Function to find if there exists a triplet in the 
+    //array A[] which sums up to X.
+    bool find3Numbers(int arr[], int n, int x)
+    {
+        //Your Code Here
+        sort(arr,arr+n);
+        for(int i=0;i<n;i++)
+        {
+           int l=i+1;
+           int r=n-1;
+           while(l<r)
+           {
+               int sum=arr[i]+arr[l]+arr[r];
+               if(sum<x)
+               l++;
+               else if(sum>x)
+               r--;
+               else
+               return true;
+           }
+        }
+        return false;
+    }
+
+};
